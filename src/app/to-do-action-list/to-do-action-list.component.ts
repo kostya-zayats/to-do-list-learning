f75bd0937fe9cb38a17b5c2f9f45e925522e6471
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2020-06-09 14:21:11 +0300

    sort

--- src/app/to-do-action-list/to-do-action-list.component.ts
+++ src/app/to-do-action-list/to-do-action-list.component.ts
@@ -8,6 +8,7 @@ import { ToDoService } from './../to-do.service';
 })
 export class ToDoActionListComponent implements OnInit {
 
+  public checkSort = false;
   public date = Date.now();
   public toDoList = this.toDoService.todos;
   constructor(public toDoService: ToDoService) { }
@@ -15,9 +16,15 @@ export class ToDoActionListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  sortData() {
+  sortLowDate() {
     return this.toDoList.sort((a, b) => {
       return <any>new Date(b.date) - <any>new Date(a.date);
+    });
+  }
+
+  sortUpDate() {
+    return this.toDoList.sort((a, b) => {
+      return <any>new Date(a.date) - <any>new Date(b.date);
     });
   }
 }
